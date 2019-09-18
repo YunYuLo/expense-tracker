1688a4e625acc22fa1ab6c1a7655282cbfcf8992
Vivian Lo <yunyubee@gmail.com>
2019-09-19 00:39:49 +0800

    Add google login funcionality and remove google secret key

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const LocalStrategy = require('passport-local').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
@@ -37,7 +38,7 @@ module.exports = passport => {
     User.findOne({ email: profile._json.email })
       .then(user => {
         if (!user) {
-          let randomPassword = Math.random().toString(36).slice(-8)
+          const randomPassword = Math.random().toString(36).slice(-8)
           bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(randomPassword, salt, (err, hash) => {
               let newUser = new User({
@@ -55,10 +56,59 @@ module.exports = passport => {
             })
           })
         } else {
-          return done(null, user)
+          //this email already exist in User
+          user['facebookId'] = profile.id
+          user.save()
+            .then(user => {
+              return done(null, user)
+            }).catch(err => {
+              console.log(err)
+            })
+
         }
       })
   }))
+
+  //google strategy
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ email: profile._json.email })
+      .then(user => {
+        if (!user) {
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(randomPassword, salt, (err, hash) => {
+              const newUser = new User({
+                name: profile._json.name,
+                email: profile._json.email,
+                password: hash,
+                googleId: profile.id
+              })
+              newUser.save()
+                .then(user => {
+                  return done(null, user)
+                }).catch(err => {
+                  console.log(err)
+                })
+            })
+          })
+        } else {
+          //this email already exist in User
+          user['googleId'] = profile.id
+          user.save()
+            .then(user => {
+              return done(null, user)
+            }).catch(err => {
+              console.log(err)
+            })
+        }
+      })
+  })
+  )
 
 
   passport.serializeUser((user, done) => {
