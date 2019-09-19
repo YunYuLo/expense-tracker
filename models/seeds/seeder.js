4e6ed559f49f1b0e24245fe99a6aecc8ad1d68e1
Vivian Lo <yunyubee@gmail.com>
2019-09-19 18:07:17 +0800

    Build seeder data with userID

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const Record = require('../record')
+const User = require('../user')
 const recordsList = require('../data/record.json').results
+const userJson = require('../data/user.json').results
+
 
 mongoose.connect('mongodb://127.0.0.1/record', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -10,8 +14,27 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 
-  // console.log(...recordsList)
-  Record.create(...recordsList)
+  for (let i = 0; i < userJson.length; i++) {
+    //create a user
+    const user = User(userJson[i])
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash
+        user.save()
+          .then()
+          .catch(err => { console.log(err) })
+      })
+    })
 
-  console.log('Record insert DB done')
+    //create expense data
+    for (let j = 0; j < recordsList.length; j++) {
+      Record.create({
+        ...recordsList[j],
+        userId: user._id
+      })
+      if (j === recordsList.length) return
+    }
+  }
+
+  console.log('Record and User insert DB done')
 })
